4d66a46fc969fa3d969b8d686fe206d089ea2eb3
alexichen <alexichen@tencent.com>
2021-04-07 10:41:40 +0800

    同步代码

---
+++ TCICSDK.framework/Headers/TCICClassController.h
@@ -0,0 +1,39 @@
+//
+//  TCICClassController.h
+//  TIC_Web_SaaS_SDK
+//
+//  Created by AlexiChen on 2020/5/6.
+//  Copyright © 2020 AlexiChen. All rights reserved.
+//
+// TCICSDKDebugVersion : 1.0.0.426
+// TCICSDKGitCommitID : a10a577e5026d939a60f4d7078c3e9a4be135c5c
+
+#import <UIKit/UIKit.h>
+#import "TCICClassConfig.h"
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+extern NSString *_Nonnull const TCIC_SDK_Version;
+extern NSString *_Nonnull const TCIC_SDK_CommitID;
+
+@interface TCICClassController : UIViewController
+
+// 使用+classRoomWithConfig: 进行构建
++ (instancetype)new  __attribute__((unavailable("Use +classRoomWithConfig: instead")));
+- (instancetype)init __attribute__((unavailable("Use +classRoomWithConfig: instead")));
+
+
+
+/**
+* 使用H5+Native方式使用，请在主线程中调用
+* @param roomConfig : 打开教室所需要的配置
+* 如果参数不合法，会返回nil，外部注意处理
+*/
+
++ (instancetype _Nullable)classRoomWithConfig:(TCICClassConfig * _Nonnull)roomConfig;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
